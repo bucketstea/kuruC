f1df2e3c0afdc4f8629b37f7ee1b10149df5ad53
bucketstea <c.g.jung.2202@gmail.com>
2024-03-23 21:35:05 +0900

    first

--- main.c
+++ main.c
@@ -4,9 +4,13 @@ int main(void){
 	int value;
 	int i;
 	int j;
+	int i1;
+	int i2;
 	value = 100;
 	i = 40;
 	j = 13;
+	i1 = i/j;
+	i2 = i%j;
 	printf("hello,\tword\n");
 	printf("%d人+%d人=%d人\n", 123, 77, 123+77);
 	printf("%d\n", (1+value) * value / 2);
@@ -15,7 +19,7 @@ int main(void){
 	printf("%f\n", 10.0 * 3.0);
 	printf("%f\n", 10.0 / 3.0);
 	printf("i=%d\n", i);
-	printf("%d ÷ %d = %d 余り%d\n", i, j, i / j, i % j);
+	printf("%d ÷ %d = %d 余り%d\n", i, j, i1, i2);
 	printf("end\n");
 	return 0;
 }
